86c663e8b9402471d15683b8e84604dc741f33fa
Антон Денисенко <adenrusbro@ya.ru>
2017-07-01 09:05:03 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+При изменении списка групп, необходимо запустить этот файл
+"""
+
+from shelver import Shelver
+from config import database_file, categories, cat_entries
+
 list_categories = categories.split(',')
 list_entries = cat_entries.split('\n')
 storage = Shelver(database_file)
@@ -10,7 +19,3 @@ else:
             storage.append(list_categories[i], list_entries[i].split(',')[j].strip(), strict=False)
 
 print(len(storage.get('Новости')[1]))
-
-
-
-
